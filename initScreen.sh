1206613503b5de31f9421f7d3fa2acb201701982
Enrique Fermin <athilaelhuno@gmail.com>
2014-06-03 09:42:15 -0430

    Se reducen los tiempos de espera

--- initScreen.sh
+++ initScreen.sh
@@ -7,21 +7,21 @@
 		echo "***********Excute Presence screen*******************"
 		echo "****************************************************"
 		sudo screen -dmS Presence java -cp lib/asterisk-java-1.0.0.M3.jar:lib/postgresql-9.1-903.jdbc4.jar:. com.capanicus.callcenter.PresenceMonitor
-		sleep 10
+		sleep 3
 
 		
 		echo "****************************************************"
 		echo "***********Excute Billing screen*******************"
 		echo "****************************************************"
 		sudo screen -dmS Billing java -cp lib/asterisk-java-1.0.0.M3.jar:lib/postgresql-9.1-903.jdbc4.jar:. com.capanicus.callcenter.LiveCallsMonitor
-		sleep 10
+		sleep 3
 
 		
 		echo "****************************************************"
 		echo "***********Excute AGI screen*******************"
 		echo "****************************************************"
 		sudo screen -dmS AGI java -cp lib/asterisk-java-1.0.0.M3.jar:lib/postgresql-9.1-903.jdbc4.jar:fastagi-mapping.properties:. -Xms1000m -Xmx1000m org.asteriskjava.fastagi.DefaultAgiServer
-		sleep 10
+		sleep 3
 
 		echo "****************************************************"
 		echo "******************View all screen*******************"
