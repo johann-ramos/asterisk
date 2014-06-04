df72865754dc82340dd855af64ff68ffa29bcb9a
Enrique Fermin <athilaelhuno@gmail.com>
2014-06-04 09:41:51 -0430

    Correcion de callcenter

--- src/update-acd/agi-bin/callcenter/src/DBConnection.java
+++ src/update-acd/agi-bin/callcenter/src/DBConnection.java
@@ -19,7 +19,8 @@ public class DBConnection
         try
         {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://10.1.189.71:9962/snpt02?user=snpt02&password=Cantv#2013");
+            //connection = DriverManager.getConnection("jdbc:postgresql://10.1.189.71:9962/snpt02?user=snpt02&password=Cantv#2013");
+              connection = DriverManager.getConnection("jdbc:postgresql://xxx.xxx.xxx.xxx:port/callcenter?user=user&password=password");
         }
         catch(Exception exception)
         {
